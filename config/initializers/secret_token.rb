30553f3c8191274d47600c968a5bf2f8a2073925
Danny Yu <dannyyu92@gmail.com>
2014-05-14 14:02:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DonationsChallenge::Application.config.secret_key_base = '95e1276a3cd1d67779c3e2886d7e19133dadae47d8577312cc0a4f1211d282be8a7d6ff90b711f2a2616dfb28f09a15c9b05e59e8cfbc46ddba5a0a42fb06722'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DonationsChallenge::Application.config.session_store :cookie_store, key: '_donations_challenge_session'
