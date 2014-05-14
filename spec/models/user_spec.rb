0a38a8f787aecbbae97308a4cd29507192e0e067
Danny Yu <dannyyu92@gmail.com>
2014-05-14 15:20:26 -0400

    Refactor leaky user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe User do
+  before :each do
+    @user = User.new(email: "testuser@example.com", password: "password")
+  end
+
   it "is valid with valid attributes" do
-    User.new(email: "testuser@example.com", password: "password").should be_valid
+    @user.should be_valid
   end
 
   it "is not valid without an email" do
-    user = User.new(email: nil, password: "password")
-    user.should_not be_valid
+    @user.email = nil
+    @user.should_not be_valid
   end
 
   it "is not valid without a password" do
-    user = User.new(email: "testuser@example.com", password: nil)
-    user.should_not be_valid
+    @user.password = nil
+    @user.should_not be_valid
   end
 end
